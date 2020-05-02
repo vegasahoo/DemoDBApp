a62fc47c824338a71ac4f339dd4e562b8d043b01
vegasahoo <shivvchandan@gmail.com>
2020-05-02 13:05:28 +0530

    This will now add 3 numbers, documentation added

--- addition.java
+++ addition.java
@@ -1,8 +1,10 @@
+//This will add 3 numbers
 Public class Addition{
 	public static void main(String[] args){
 		
 		int a = 10;
 		int b = 20;
-		System.out.println(a+b);
+		int c =30;
+		System.out.println(a+b+c);
 	}
 }
