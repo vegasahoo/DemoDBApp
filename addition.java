6eeb1ddec12a77dd21480aba2b66e7a4b62edccc
vegasahoo <shivvchandan@gmail.com>
2020-05-02 13:12:56 +0530

    Revert "This will now add 3 numbers, documentation added"
    This reverts commit a62fc47c824338a71ac4f339dd4e562b8d043b01.

--- addition.java
+++ addition.java
@@ -1,10 +1,8 @@
-//This will add 3 numbers
 Public class Addition{
 	public static void main(String[] args){
 		
 		int a = 10;
 		int b = 20;
-		int c =30;
-		System.out.println(a+b+c);
+		System.out.println(a+b);
 	}
 }
